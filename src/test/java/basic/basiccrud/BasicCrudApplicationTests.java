0d29fea01b3192c689022af1f8dfe3cb269afd8f
JoNamKeun <dipper25@nate.com>
2023-03-14 00:23:05 +0900

    create basic project

---
+++ src/test/java/basic/basiccrud/BasicCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.basiccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
